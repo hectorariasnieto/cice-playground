1924cbc59173b33df81ecb4ff6c406374c023ef5
hector.arias.nieto@gmail.com <#P3licula>
2020-12-11 16:23:15 +0100

    main.js

--- packages/6-webapp/fetch/main.js
+++ packages/6-webapp/fetch/main.js
@@ -1,8 +1,8 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
-  .then((json) => console.log(json))
-
-for (let post in json){
-  var lista = document.createElement("li");
-  lista.innerHTML = post.value;
+  .then((json) => {
+    for (let post in json){
+      var lista = document.createElement("li");
+      lista.innerHTML = post.value;
+  })
 }
