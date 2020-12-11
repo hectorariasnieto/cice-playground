3f48c0f0648b432844544d9ba8266bd54a5d923a
hector.arias.nieto@gmail.com <#P3licula>
2020-12-11 19:51:46 +0100

    new file

---
+++ packages/6-webapp/fetch/trycatch/main.js
@@ -0,0 +1,34 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(result => {
+    const titles = result.map(post => post.title)
+    titles.forEach(title => {
+      const li = document.createElement('li')
+      const postsElement = document.querySelector('#posts')
+      li.innerText = title
+      postsElement.appendChild(li)
+    })
+  })
+
+const createPost = document.querySelector('#create-post')
+createPost.addEventListener('click', () => {
+  const post = {
+    title: 'foo',
+    id: 1,
+    body: 'bar'
+  }
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(post, null, 2)
+  })
+})
+
+function throwError(){
+  throw new Error('wtf?')
+}
+try {
+  throwError()
+} catch (e) {
+  expect(e).toEqual(new Error("wtf?"))
+}
+
